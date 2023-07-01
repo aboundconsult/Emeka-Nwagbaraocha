62013b467d2ddb60f52f093d9337b68872bfb181
Emmanuel <emanuelanyigor@gmail.com>
2023-06-30 21:35:58 -0700

    redesigned the navbar for mobile screen

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,62 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./navbar.css";
+import './navbar.css';
 
-const Navbar = () => {
-    const [ active, setActive ] = useState(false);
+export default function Navbar(){
     const { pathname } = useLocation();
+    const [ open , set_open ] = useState(false);
 
     useEffect(()=> {
         window.addEventListener("resize", () => {
-            if(window.innerWidth > 824){
-                setActive(false);
+            if(window.innerWidth > 823){
+                set_open(false);
             }
         });
     }, []);
 
     return (
-        <nav className={`app__navbar ${ active ? "-active" : ""}`}>
-            <div className="app__navbar-container app__container">
-                <div className="app__navbar-row d-flex item-center justify-between gap-2">
-                    <div className="app__navbar-col -left">
-                        <Link to="/">
-                            <div className="-logo">
-                                <h4> Emeka <br /> <span> Nwagbaraocha </span> </h4>
+        <nav className={`app__navbar ${open ? "-open" : ""}`}>
+            <div className={`app__navbar-bg`}></div>
+            <div className="app__navbar-fill"></div>
+            <div className="app__navbar-toggle">
+                <button className="app__navbar-toggle_menu" onClick={()=> set_open(!open)}>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+            <div className="app__navbar-content app__container">
+                <div className="app__navbar-container">
+                    <div className="app__navbar-container_row">
+                        <div className="app__navbar-container_col -left">
+                            <div className="app__navbar-logo -static">
+                                <Link to="/">
+                                    <div className="-logo">
+                                        <h4> Emeka <br /> <span> Nwagbaraocha </span> </h4>
+                                    </div>
+                                </Link>
                             </div>
-                        </Link>
-                    </div>
-                    <div className="app__navbar-col -right">
-                        <ul className="app__navbar-nav_items d-flex gap-2 item-center">
-                            <li className="app__navbar-nav_item">
-                                <Link to="/" className={pathname === "/" ? '-active' : ""}>
+                        </div>
+                        <div className="app__navbar-container_col -right">
+                            <div className="app__navbar-nav">
+                                <Link to="/" className={`app__navbar-navItem ${pathname === "/" ? "-active" : "" }`} onClick={()=> set_open(false)}>
                                     <span> Home </span>
                                 </Link>
-                            </li>
-                            <li className="app__navbar-nav_item">
-                                <Link to="/about" className={pathname === "/about" ? '-active' : ""}>
+                                <Link to="/about" className={`app__navbar-navItem ${pathname === "/about" ? "-active" : "" }`} onClick={()=> set_open(false)}>
                                     <span> About Me </span>
                                 </Link>
-                            </li>
-                            <li className="app__navbar-nav_item">
-                                <Link to="/media" className={pathname === "/media" ? '-active' : ""}>
-                                    <span> Media </span>
-                                </Link>
-                            </li>
-                            <li className="app__navbar-nav_item">
-                                <Link to="/gallery" className={pathname === "/gallery" ? '-active' : ""}>
+                                <Link to="/gallery" className={`app__navbar-navItem ${pathname === "/gallery" ? "-active" : "" }`} onClick={()=> set_open(false)}>
                                     <span> Gallery </span>
                                 </Link>
-                            </li>
-                            <li className="app__navbar-nav_item">
-                                <Link to="/contact" className={pathname === "/contact" ? '-active' : ""}>
+                                <Link to="/media" className={`app__navbar-navItem ${pathname === "/media" ? "-active" : "" }`} onClick={()=> set_open(false)}>
+                                    <span> Media </span>
+                                </Link>
+                                <Link to="/contact" className={`app__navbar-navItem ${pathname === "/contact" ? "-active" : "" }`} onClick={()=> set_open(false)}>
                                     <span> Contact </span>
                                 </Link>
-                            </li>
-                        </ul>
-
-                        <div className="hamburger d-flex flex-column" onClick={() => setActive(!active)}>
-                            <span className="strand"></span>
-                            <span className="strand"></span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -64,5 +61,3 @@ const Navbar = () => {
         </nav>
     )
 }
-
-export default Navbar
